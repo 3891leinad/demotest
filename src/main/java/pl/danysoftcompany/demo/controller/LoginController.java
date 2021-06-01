49c7d7da6e176f00234053dc227806e172401828
daniel.dellpc <d18@go2.pl>
2021-06-01 07:06:39 +0200

    Added spring security - authentication by json

---
+++ src/main/java/pl/danysoftcompany/demo/controller/LoginController.java
@@ -0,0 +1,14 @@
+package pl.danysoftcompany.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.danysoftcompany.demo.config.LoginCredentials;
+
+@RestController
+public class LoginController {
+    @PostMapping("/login")
+    public void login(@RequestBody LoginCredentials credentials) {
+
+    }
+}
